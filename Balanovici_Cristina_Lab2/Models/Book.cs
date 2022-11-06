b936193af6f6e5edde98cf04eac57b6e4425abc2
Cristina Balanovici <cristina.balanovici@yahoo.com>
2022-11-06 17:01:32 +0200

    Add project files.

---
+++ Balanovici_Cristina_Lab2/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Balanovici_Cristina_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public int AuthorID { get; set; }
+        public ICollection<Order> Orders { get; set; }
+        public Author Author { get; set; }
+        
+       
+    }
+}
